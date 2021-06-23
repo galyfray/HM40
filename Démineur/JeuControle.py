9f3962f1e6234ebd59b84e2613aaec51d9ae5c80
Galyfray <galyfray@gmail.com>
2021-06-23 12:53:55 +0200

    fix démineur

--- Démineur/JeuControle.py
+++ Démineur/JeuControle.py
@@ -1,13 +1,16 @@
-import Jeu
-#import bib
+# import bib
+# import bib
 import tkinter as tk
-#création fenêtre
+
+import Jeu
+
+# création fenêtre
 fen = tk.Tk()
-#titre
+# titre
 fen.title("Démineur")
-#taille
-fen.geometry("150x240")
-fen.resizable(width=False, height=False)
+# taille
+# fen.geometry("150x240")
+
 frame_Top = tk.Frame(fen)
 frame_Top.pack()
 frame_general = tk.Frame(fen)
@@ -15,17 +18,21 @@ frame_general.pack()
 frame_bottom = tk.Frame(fen)
 frame_bottom.pack()
 frame_result = tk.Frame(fen)
-quitter = tk.Button(frame_bottom,text="Quitter",command= fen.destroy, relief=tk.GROOVE)
+quitter = tk.Button(frame_bottom, text="Quitter", command=fen.destroy, relief=tk.GROOVE)
 quitter.grid(row=0, column=0)
 label_titre_lose = tk.Label(frame_result, text="Perdu Cheh !", height=2, relief=tk.SUNKEN, fg='black',
-                               font=("Calibri", 18))
+                            font=("Calibri", 18))
 label_titre_win = tk.Label(frame_result, text="GG Gagné !", height=2, relief=tk.SUNKEN, fg='black',
-                               font=("Calibri", 18))
-game =0
-def NewGame(w,h,m):
-      global game
-      game = Jeu.Minefield(w, h, m)
-      affichage()
+                           font=("Calibri", 18))
+game = 0
+fen.resizable(width=False, height=False)
+
+
+def NewGame(w, h, m):
+    global game
+    game = Jeu.Minefield(w, h, m)
+    affichage()
+
 
 def Reset():
     frame_bottom.pack_forget()
@@ -35,19 +42,24 @@ def Reset():
     label_titre_win.pack_forget()
     label_titre_lose.pack_forget()
     fen.geometry("150x240")
-    NewGame(8,8,10)
+    NewGame(8, 8, 10)
 
-reset = tk.Button(frame_bottom,text="Recommencer", command = Reset, relief=tk.GROOVE)
+
+reset = tk.Button(frame_bottom, text="Recommencer", command=Reset, relief=tk.GROOVE)
 reset.grid(row=0, column=1)
-def hit(x,y):
+
+
+def hit(x, y):
     print("Hit:")
     print(str(x) + "/" + str(y))
     global game
-    game.revelation(x,y)
+    game.revelation(x, y)
 
     affichage()
+
+
 def victory0rLose():
-    i=1
+    i = 1
     print("Explosion" + str(game.get_explosions()))
     if game.get_explosions() != 0:
         # ajout texte
@@ -58,7 +70,7 @@ def victory0rLose():
         frame_bottom.pack()
         fen.geometry("150x100")
         return 1
-    if game.get_reaveled() == game.fieldMines and game.get_explosions() ==0:
+    if game.get_reaveled() == game.fieldMines and game.get_explosions() == 0:
         frame_general.pack_forget()
         frame_bottom.pack_forget()
         label_titre_win.pack()
@@ -66,6 +78,8 @@ def victory0rLose():
         frame_bottom.pack()
         fen.geometry("150x100")
         return 1
+
+
 def affichage():
     global game
     if victory0rLose() != 0:
